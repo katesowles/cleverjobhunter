b8dbe47d38b2223d659c0658709824cf66bf925a
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-09-13 16:18:00 -0700

    fix header link

--- src/routes.js
+++ src/routes.js
@@ -155,7 +155,7 @@ export default function configRoutes($stateProvider, $urlRouterProvider) {
       },
       views: {
         header: {
-          component: 'headerLogin'
+          component: 'header'
         },
         main: {
           component: 'listUsers'
