75fd2566ca8d33cac435519602e8601bc1295188
Danielle H <daheber@gmail.com>
2016-09-13 12:55:12 -0700

    enter link for login

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,13 +5,30 @@ export default {
   controller
 };
 
-controller.$inject = ['userService', '$state'];
+controller.$inject = ['userService', '$state', '$mdDialog'];
 
-function controller(userService, $state) {
+function controller(userService, $state, $mdDialog) {
   this.username = 'Name-Goes-Here';
   this.logout = ()=>{
     userService.logout();
     $state.go('home');
   };
   this.isAuthenticated = userService.isAuthenticated;
+  this.prompt = ()=>{
+    $mdDialog.show({
+      parent: angular.element(document.body),
+      template: '<user-auth success="success()" cancel="cancel()"></user-auth>',
+      controller: ['$scope', function($scope) {
+        $scope.success = function(){
+          $mdDialog.hide();
+          return $state.go('dashboard');
+        };
+        $scope.cancel = () => {
+          $mdDialog.hide();
+        };
+      }],
+      clickOutsideToClose: true,
+      escapeToClose: true
+    });
+  };
 };
