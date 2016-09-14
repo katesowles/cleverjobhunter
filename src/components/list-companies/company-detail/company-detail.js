601823ad2595b4664549fde6f93b6d11da852718
Danielle H <daheber@gmail.com>
2016-09-14 14:48:46 -0700

    edit company form

--- src/components/list-companies/company-detail/company-detail.js
+++ src/components/list-companies/company-detail/company-detail.js
@@ -12,8 +12,8 @@ export default {
 
 
 
-controller.$inject = ['companyService', '$window', '$state'];
-function controller(companyService, $window, $state){
+controller.$inject = ['$mdDialog', 'companyService', '$window', '$state'];
+function controller($mdDialog, companyService, $window, $state){
   this.styles = styles;
   this.userId = $window.localStorage['id'];
 
@@ -21,24 +21,30 @@ function controller(companyService, $window, $state){
   companyService.get($state.params.companyId)
     .then(company => {
       this.company = company;
-      console.log(this.company);
     })
     .catch(err => console.log(err));
 
+  this.edit = ()=>{
+    const parentEl = angular.element(document.body);
+    $mdDialog.show({
+      parent: parentEl,
+      controllerAs: '$ctrl',
+      bindToController: true,
+      template: '<edit-company company="$ctrl.company"></edit-company>',
+      controller(){},
+      locals: {
+        company: this.company
+      },
+      clickOutsideToClose: true,
+      escapeToClose: true
+    })
+    .then(updatedCompany => {
+      if (!updatedCompany) return;
+      //pass copied and updated version to original
+      angular.copy(updatedCompany, this.company);
+    });
+  };
+
 }
 
-
-  // this.company = {
-  //   name: 'Apple Inc.',
-  //   service: 'Computer hardware/software',
-  //   location: 'Cupertino, CA',
-  //   info: 'This is a company that built the iPhone and Mac OSX. They are also involved in selling digitial media view the iTunes and Mac App stores.',
-  //   tech: 'iOS, Mac OSX',
-  //   pros: ['pro1', 'pro2', 'pro3'],
-  //   cons: ['con1', 'con2'],
-  //   questions: ['question1', 'question2'],
-  //   contacts: [{_id: '123'}, {_id: '456'}],
-  //   links: [{url: 'http://apple.com', desc: 'Apple homepage'}],
-  //   actionItems: [{date: 'Aug. 10, 2016', plan: 'Applied for job'}, {date: 'Sept. 1, 2016', plan: 'First round interview'}]
-  // };
 
